e4f7fad48c874d2ab54c9c8a4e2528626093a67b
AinaraGarzo <ainara.garzo@gmail.com>
2022-10-16 12:32:34 +0200

    smtp configuration

--- forms/contact.php
+++ forms/contact.php
@@ -25,8 +25,8 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   $contact->smtp = array(
-    'host' => 'smtp.mail.yahoo.com',
-    'username' => 'aigama@yahoo.es',
+    'host' => 'smtp.gmail.com',
+    'username' => 'ainara.garzo@gmail.com',
     'password' => 'uvglwhoskhdvvyfe',
     'port' => '587'
   );
