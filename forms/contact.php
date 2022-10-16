9a7cecbb93b5ff8f9b859f37cedaae36eb5bc2db
AinaraGarzo <ainara.garzo@gmail.com>
2022-10-16 12:47:42 +0200

    remove contact form

--- forms/contact.php
+++ forms/contact.php
@@ -24,12 +24,13 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
   $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
-    'username' => 'ainara.garzo@gmail.com',
-    'password' => 'uvglwhoskhdvvyfe',
+    'host' => 'smtp host',
+    'username' => 'example@mail.com',
+    'password' => 'pass',
     'port' => '587'
-  );
+  );*/
   
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
